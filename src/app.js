4254b468b73d88df0b5a3906e5c1559a5da26480
ycode2023 <133668282+ycode2023@users.noreply.github.com>
2023-06-01 13:02:34 +0100

    added OneCall Api

--- src/app.js
+++ src/app.js
@@ -19,7 +19,8 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
     
     let days = ["Weds", "Thurs", "Fri", "Sat", "Sun"];
@@ -49,8 +50,8 @@ function displayForecast() {
 
 function getForecast(coordinates){
     let apiKey = "62231151ce343c4d68652e1617efc22f";
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature (response) {
@@ -120,4 +121,3 @@ let celciusLink = document.querySelector("#celcius");
 celcius.addEventListener("click", displayCelcius);
 
 search("London");
-displayForecast();
